72d4a55d9908364fe18f2978799ab9615b9dfb28
Crazycaleb <52386586+Crazycaleb@users.noreply.github.com>
2023-07-16 22:02:19 -0400

    Initial Commit

---
+++ Assets/SetTheoryScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+using KModkit;
+using NUnit.Framework.Constraints;
+
+public class SetTheoryScript : MonoBehaviour
+{
+    public KMBombModule Module;
+    public KMBombInfo BombInfo;
+    public KMAudio Audio;
+
+    public KMSelectable[] Buttonage;
+    public KMSelectable submit;
+    public Sprite[] set;
+
+    private int _moduleId;
+    private static int _moduleIdCounter = 1;
+    private bool _moduleSolved;
+    
+
+    private void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+    }
+}
