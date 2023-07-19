8e1bfb496402ef1b2d37f67937563cea31328e7f
Timwi <github@timwi.de>
2023-07-19 02:25:56 +0200

    Implement equation generator

---
+++ Assets/EquationTerm.cs
@@ -0,0 +1,105 @@
+﻿using System.Linq;
+using System;
+
+namespace SetTheory
+{
+    public enum SetSymbol
+    {
+        Pacman,
+        X,
+        Triangle,
+        Teepee,
+        Shirt,
+        Arrow,
+        Diamond,
+        H,
+        Star
+    }
+
+    public enum SetOperation
+    {
+        Union,
+        Intersection,
+        Difference,
+        SymmetricDifference,
+        Complement
+    }
+
+    abstract class EquationTerm : object
+    {
+        public abstract SetSymbol[] Evaluate(SetSymbol[][] variables);
+    }
+
+    class EquationTermVariable : EquationTerm
+    {
+        public int VariableIndex { get; private set; }
+
+        public EquationTermVariable(int index)
+        {
+            VariableIndex = index;
+        }
+
+        public override SetSymbol[] Evaluate(SetSymbol[][] variables)
+        {
+            return variables[VariableIndex];
+        }
+
+        public override string ToString()
+        {
+            return "ABC"[VariableIndex].ToString();
+        }
+    }
+
+    class EquationTermOperation : EquationTerm
+    {
+        public SetOperation Operation { get; private set; }
+        public EquationTerm Left { get; private set; }
+        public EquationTerm Right { get; private set; }
+
+        public EquationTermOperation(SetOperation op, EquationTerm left, EquationTerm right = null)
+        {
+            Operation = op;
+            Left = left;
+            Right = right;
+        }
+
+        private SetSymbol[] CalculateOperation(SetOperation op, SetSymbol[] a, SetSymbol[] b)
+        {
+            switch (op)
+            {
+                case SetOperation.Union:
+                    return a.Union(b).ToArray();
+                case SetOperation.Intersection:
+                    return a.Intersect(b).ToArray();
+                case SetOperation.Difference:
+                    return a.Except(b).ToArray();
+                case SetOperation.SymmetricDifference:
+                    return ((SetSymbol[]) Enum.GetValues(typeof(SetSymbol))).Where(i => a.Contains(i) != b.Contains(i)).ToArray();
+                case SetOperation.Complement:
+                    return ((SetSymbol[]) Enum.GetValues(typeof(SetSymbol))).Where(i => !a.Contains(i)).ToArray();
+                default:
+                    throw new InvalidOperationException("Invalid set operation.");
+            }
+        }
+
+        public override SetSymbol[] Evaluate(SetSymbol[][] variables)
+        {
+            return CalculateOperation(Operation, Left.Evaluate(variables), Right == null ? null : Right.Evaluate(variables));
+        }
+
+        public override string ToString()
+        {
+            string opSymbol;
+            switch (Operation)
+            {
+                case SetOperation.Union: opSymbol = "∪"; break;
+                case SetOperation.Intersection: opSymbol = "∩"; break;
+                case SetOperation.Difference: opSymbol = "−"; break;
+                case SetOperation.SymmetricDifference: opSymbol = "∆"; break;
+                case SetOperation.Complement: return "!" + Left.ToString();
+                default: throw new InvalidOperationException("Invalid set operation.");
+            }
+            return string.Format("({0} {1} {2})", Left, opSymbol, Right);
+        }
+    }
+}
--- Assets/SetTheoryScript.cs
+++ Assets/SetTheoryScript.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
-using Rnd = UnityEngine.Random;
 using KModkit;
+using SetTheory;
+
+using Rnd = UnityEngine.Random;
 
 public class SetTheoryScript : MonoBehaviour
 {
@@ -18,37 +20,22 @@ public class SetTheoryScript : MonoBehaviour
     public Sprite[] SetSprites;
     public SpriteRenderer[] ButtonSprites;
 
+    public TextMesh Display;
+
     private int _moduleId;
     private static int _moduleIdCounter = 1;
     private bool _moduleSolved;
 
-    public enum SetSymbol
-    {
-        Pacman,
-        X,
-        Triangle,
-        Teepee,
-        Shirt,
-        Arrow,
-        Diamond,
-        H,
-        Star
-    }
-
-    public enum SetOperations
-    {
-        Union,
-        Intersection,
-        Difference,
-        SymmetricDifference,
-        Complement
-    }
-
-    private SetSymbol[] _buttonSprites;
+    private SetSymbol[] _buttonSymbols;
 
     private SetSymbol[] _setASymbols;
     private SetSymbol[] _setBSymbols;
     private SetSymbol[] _setCSymbols;
+
+    private EquationTerm[] _equations;
+    private SetSymbol[][] _solutions;
+    private int _stage;
+    private HashSet<SetSymbol> _curInput = new HashSet<SetSymbol>();
 
     private void Start()
     {
@@ -57,50 +44,95 @@ public class SetTheoryScript : MonoBehaviour
             ButtonSels[i].OnInteract += ButtonPress(i);
         SubmitSel.OnInteract += SubmitPress;
 
-        _buttonSprites = (SetSymbol[])Enum.GetValues(typeof(SetSymbol)).Shuffle();
-        for (int i = 0; i < _buttonSprites.Length; i++)
-            ButtonSprites[i].sprite = SetSprites[(int)_buttonSprites[i]];
+        _buttonSymbols = (SetSymbol[]) Enum.GetValues(typeof(SetSymbol)).Shuffle();
+        for (int i = 0; i < _buttonSymbols.Length; i++)
+            ButtonSprites[i].sprite = SetSprites[(int) _buttonSymbols[i]];
 
         // Set A
-        _setASymbols = _buttonSprites.Where(i => SetAValidity(i)).ToArray();
+        _setASymbols = _buttonSymbols.Where(i => SetAValidity(i)).ToArray();
         Debug.LogFormat("[S.E.T. Theory #{0}] Symbols present in Set A: {1}", _moduleId, _setASymbols.Join(", "));
 
         // Set B
         var setBPositions = new SetSymbol[] { SetSymbol.H, SetSymbol.Pacman, SetSymbol.Arrow, SetSymbol.Diamond, SetSymbol.X, SetSymbol.Star, SetSymbol.Teepee, SetSymbol.Triangle, SetSymbol.Shirt };
-        var validSetBPartOne = Enumerable.Range(0, 9).Select(i => _buttonSprites[i] == setBPositions[i]).ToArray();
+        var validSetBPartOne = Enumerable.Range(0, 9).Select(i => _buttonSymbols[i] == setBPositions[i]).ToArray();
         var setBSnChars = new string[] { "WO4", "BS3", "HA1", "TR5", "CN2", "EX9", "LU7", "FI6", "PJ8" };
         var validSetBPartTwo = setBSnChars.Select(i => i.All(j => !BombInfo.GetSerialNumber().Contains(j))).ToArray();
 
-        _setBSymbols = Enumerable.Range(0, 9).Where(i => validSetBPartOne[i] != validSetBPartTwo[i]).Select(i => _buttonSprites[i]).ToArray();
+        _setBSymbols = Enumerable.Range(0, 9).Where(i => validSetBPartOne[i] != validSetBPartTwo[i]).Select(i => _buttonSymbols[i]).ToArray();
         Debug.LogFormat("[S.E.T. Theory #{0}] Symbols present in Set B: {1}", _moduleId, _setBSymbols.Join(", "));
 
         // Set C
         var listC = new List<SetSymbol>();
-        int index = (int)_buttonSprites[8];
+        int index = (int) _buttonSymbols[8];
         int x = index % 3;
         int y = index / 3;
-        listC.Add(_buttonSprites[8]);
+        listC.Add(_buttonSymbols[8]);
         for (int i = 7; i >= 0; i--)
         {
-            int ix = (int)_buttonSprites[i];
+            int ix = (int) _buttonSymbols[i];
             if (ix == 4)
                 continue;
             int dx = (ix % 3) - 1;
             int dy = (ix / 3) - 1;
             x = (x + dx + 3) % 3;
             y = (y + dy + 3) % 3;
-            SetSymbol val = (SetSymbol)(y * 3 + x);
+            SetSymbol val = (SetSymbol) (y * 3 + x);
             if (listC.Contains(val))
                 break;
             listC.Add(val);
         }
         _setCSymbols = listC.ToArray();
         Debug.LogFormat("[S.E.T. Theory #{0}] Symbols present in Set C: {1}", _moduleId, _setCSymbols.Join(", "));
+
+        _equations = Enumerable.Range(0, 4).Select(GenerateEquationForStage).ToArray();
+        var sets = new[] { _setASymbols, _setBSymbols, _setCSymbols };
+        _solutions = _equations.Select(eq => eq.Evaluate(sets)).ToArray();
+
+        for (var stage = 0; stage < 4; stage++)
+        {
+            Debug.LogFormat("[S.E.T. Theory #{0}] Stage {1} equation: {2}", _moduleId, stage + 1, _equations[stage]);
+            Debug.LogFormat("[S.E.T. Theory #{0}] Stage {1} solution: {2}", _moduleId, stage + 1, _solutions[stage].Join(", "));
+        }
+
+        DisplayStage(_stage);
+    }
+
+    private void DisplayStage(int stage)
+    {
+        Display.text = _equations[stage].ToString();
+
+        for (var btn = 0; btn < 9; btn++)
+        {
+            if (_curInput.Contains(_buttonSymbols[btn]))
+                ;   // TODO: depress this button
+            else
+                ;   // TODO: un-depress this button
+        }
     }
 
     private bool SubmitPress()
     {
-        // do more stuff
+        if (_curInput.SetEquals(_solutions[_stage]))
+        {
+            Debug.LogFormat("[S.E.T. Theory #{0}] Stage {1} solved.", _moduleId, _stage + 1);
+            _stage++;
+            if (_stage == 4)
+            {
+                Module.HandlePass();
+                Debug.LogFormat("[S.E.T. Theory #{0}] Module solved.", _moduleId);
+            }
+            else
+            {
+                _curInput.Clear();
+                DisplayStage(_stage);
+            }
+        }
+        else
+        {
+            Module.HandleStrike();
+            Debug.LogFormat("[S.E.T. Theory #{0}] In stage {1}, you submitted {2}. Strike!", _moduleId, _stage + 1, _curInput.Join(", "));
+        }
+
         return false;
     }
 
@@ -108,7 +140,17 @@ public class SetTheoryScript : MonoBehaviour
     {
         return delegate ()
         {
-            // do stuff
+            if (_moduleSolved)
+                return false;
+
+            var symbolPressed = _buttonSymbols[i];
+            if (_curInput.Contains(symbolPressed))
+                _curInput.Remove(symbolPressed);
+            else
+                _curInput.Add(symbolPressed);
+            DisplayStage(_stage);
+
+            Debug.LogFormat("<> {0}", _curInput.Join(", "));
             return false;
         };
     }
@@ -131,22 +173,49 @@ public class SetTheoryScript : MonoBehaviour
         return false;
     }
 
-    private SetSymbol[] CalculateOperation(SetOperations op, SetSymbol[] a, SetSymbol[] b = null)
+    private EquationTerm GenerateEquationForStage(int stage)
     {
-        switch (op)
+        var variables = new[] { 0, 1, 2 }
+            .Select(i => (EquationTerm) new EquationTermVariable(i))
+            .ToArray()
+            .Shuffle();
+
+        switch (stage)
         {
-            case SetOperations.Union:
-                return a.Union(b).ToArray();
-            case SetOperations.Intersection:
-                return a.Intersect(b).ToArray();
-            case SetOperations.Difference:
-                return a.Except(b).ToArray();
-            case SetOperations.SymmetricDifference:
-                return ((SetSymbol[])Enum.GetValues(typeof(SetSymbol))).Where(i => a.Contains(i) != b.Contains(i)).ToArray();
-            case SetOperations.Complement:
-                return ((SetSymbol[])Enum.GetValues(typeof(SetSymbol))).Where(i => !a.Contains(i)).ToArray();
-            default:
-                throw new InvalidOperationException("Invalid set operation.");
+            case 0:
+                // Two different variables, one operation that is not Complement
+                return GenerateSubequation(useComplement: false, terms: variables);
+
+            case 1:
+                // Two different variables, one operation that is not Complement, but a variable or the whole thing might be Complemented
+                return GenerateSubequation(useComplement: true, terms: variables);
+
+            case 2:
+                // All three variables, two operations, possibly randomly complemented
+                var subeq = GenerateSubequation(useComplement: true, terms: variables);
+                return GenerateSubequation(useComplement: true, terms: new[] { subeq, variables[2] });
+
+            case 3:
+                // All three variables plus a duplicate, three operations, possibly randomly complemented
+                var subeq1 = GenerateSubequation(useComplement: true, terms: variables);
+                var subeq2 = GenerateSubequation(useComplement: true, terms: new[] { variables[2], variables[Rnd.Range(0, 2)] });
+                return GenerateSubequation(useComplement: true, terms: new[] { subeq1, subeq2 });
+
+            default: throw new InvalidOperationException("Invalid stage.");
         }
+    }
+
+    private static EquationTerm GenerateSubequation(bool useComplement, EquationTerm[] terms)
+    {
+        var op = ((SetOperation[]) Enum.GetValues(typeof(SetOperation))).Except(new[] { SetOperation.Complement }).PickRandom();
+        var swapped = Rnd.Range(0, 2) != 0;
+        var left = swapped ? terms[1] : terms[0];
+        var right = swapped ? terms[0] : terms[1];
+        return new EquationTermOperation(op, useComplement ? RandomlyComplement(left) : left, useComplement ? RandomlyComplement(right) : right);
+    }
+
+    private static EquationTerm RandomlyComplement(EquationTerm eq)
+    {
+        return Rnd.Range(0, 2) != 0 ? new EquationTermOperation(SetOperation.Complement, eq) : eq;
     }
 }
